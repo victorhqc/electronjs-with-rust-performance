6aa1e63344aa26fc3de9d19dc2e75a4fbe118109
Victor Quiroz <victorhqc@gmail.com>
2020-07-27 12:23:08 +0200

    feat: db utilities to connect & migrate

---
+++ rust-core/src/db.rs
@@ -0,0 +1,42 @@
+use diesel::r2d2::{ConnectionManager, Pool};
+use diesel::SqliteConnection;
+use diesel_migrations::embed_migrations;
+use snafu::{ResultExt, Snafu};
+use std::env;
+
+embed_migrations!();
+
+pub fn db_pool(url: Option<String>) -> Result<DbPool> {
+  let database_url = match url {
+    Some(u) => format!("{}", u),
+    None => env::var("DATABASE_URL").unwrap_or_else(|_| "./lol_matches.db".to_string()),
+  };
+  let manager = ConnectionManager::<SqliteConnection>::new(database_url);
+
+  let pool = Pool::builder().build(manager).context(BuildPool)?;
+
+  Ok(pool)
+}
+
+pub fn db_migrate(pool: &DbPool) -> Result<()> {
+  let conn = pool.get().expect("Couldn't get DB connection");
+
+  embedded_migrations::run_with_output(&conn, &mut std::io::stdout()).context(Migration)?;
+
+  Ok(())
+}
+
+pub type DbPool = Pool<ConnectionManager<SqliteConnection>>;
+
+#[derive(Debug, Snafu)]
+pub enum DbError {
+  #[snafu(display("Could not build pool connection: {}", source))]
+  BuildPool { source: diesel::r2d2::PoolError },
+
+  #[snafu(display("Failed to run migrations: {}", source))]
+  Migration {
+    source: diesel_migrations::RunMigrationsError,
+  },
+}
+
+pub type Result<T, E = DbError> = std::result::Result<T, E>;
--- rust-core/src/lib.rs
+++ rust-core/src/lib.rs
@@ -10,6 +10,7 @@ extern crate diesel_migrations;
 #[macro_use]
 extern crate strum_macros;
 
+pub mod db;
 pub mod helpers;
 pub mod models;
 pub mod schema;
