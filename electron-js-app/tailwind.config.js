63d82b46242376e4d310a16a8b99f0d03c40e2b2
Victor Quiroz <victorhqc@gmail.com>
2020-07-29 23:06:59 +0200

    feat: electron-js-app
    Hello World Electron Application

---
+++ electron-js-app/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  purge: ['./src/**/*.ts', './src/**/*.tsx'],
+  theme: {
+    extend: {},
+    variants: {},
+    plugins: [],
+  },
+};
---
+++ electron-js-app/webpack.renderer.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const rules = require('./webpack.rules');
+const plugins = require('./webpack.plugins');
+
+rules.push({
+  test: /\.css$/,
+  use: [
+    {
+      loader: MiniCssExtractPlugin.loader,
+    },
+    { loader: 'css-loader', options: { importLoaders: 1 } },
+    {
+      loader: 'postcss-loader',
+      options: {
+        ident: 'postcss',
+        plugins: [require('tailwindcss'), require('autoprefixer')],
+      },
+    },
+  ],
+});
+
+plugins.push(
+  new MiniCssExtractPlugin({
+    filename: 'styles.css',
+    chunkFilename: 'styles.css',
+  }),
+);
+
+module.exports = {
+  module: {
+    rules,
+  },
+  plugins: plugins,
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+  },
+};
